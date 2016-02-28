28687f015318fe9cfac43acc996a31d4335cd1e8
ysl3000 <yannicklamprecht@live.de>
2016-02-28 01:14:31 +0100

    Check commit stats -> email instead username

--- src/de/ysl3000/chunkguard/commands/ToggleRegionInfo.java
+++ src/de/ysl3000/chunkguard/commands/ToggleRegionInfo.java
@@ -5,6 +5,8 @@ import org.bukkit.command.*;
 public class ToggleRegionInfo implements CommandExecutor
 {
     public boolean onCommand(final CommandSender commandSender, final Command command, final String s, final String[] strings) {
+
+
         return false;
     }
 }
